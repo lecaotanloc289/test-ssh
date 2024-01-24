5fa24e2ed631e38f6e1f8752e2ff00420d92239c
Lecaotanloc <lecaotanloc289@gmail.com>
2024-01-24 13:44:45 +0700

    update -4

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Header from './components/header/Header'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/header/Header";
 
-import Footer from './components/footer/footer' // Đảm bảo đúng đường dẫn
-import SignIn from './components/signin/SignIn'
-import Register from './components/register/register'
-import Categories from './components/categories/Categories'
-import Home from './components/home/Home'
-import Ordersuccessful from './components/ordersuccessful/ordersuccessful'
+import Footer from "./components/footer/footer"; // Đảm bảo đúng đường dẫn
+import SignIn from "./components/signin/SignIn";
+import Register from "./components/register/Register";
+import Categories from "./components/categories/Categories";
+import Home from "./components/home/Home";
+import Ordersuccessful from "./components/ordersuccessful/ordersuccessful";
 
 function App() {
-    return (
-        <Router>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/signin" element={<SignIn />} />
-                <Route path="/categories" element={<Categories />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/register" element={<Ordersuccessful />} />
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/register" element={<Ordersuccessful />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
 
-            </Routes>
-            <Footer />
-        </Router>
-    )
-}   
-
-export default App
+export default App;
